fc6ce5c77b659ff9378ffd9c0736bdf2af7700b7
Shin soomin <soomin6372@gmail.com>
2023-06-12 16:33:26 +0900

    sdd

--- Kh3/src/project1/soomin/ReservationControllerImp.java
+++ Kh3/src/project1/soomin/ReservationControllerImp.java
@@ -1,6 +1,6 @@
 package project1.soomin;
 
-public interface NotifyControllerImp {
+public interface ReservationControllerImp {
 	
  public void registerWaiting();
  	// 웨이팅 등록 알림
@@ -32,35 +32,20 @@ public interface NotifyControllerImp {
  
 
  
-// [리뷰]    
- public void notifyReviewRequest(); 
-     // 식당 리뷰 요청 알림
- 	// 사용자들에게 식당 방문 후 리뷰를 작성하도록 요청하는 알림
-
- public void notifyReviewRegistration(); 
-     // 리뷰 등록 알림
-
- public void updateReview(); 
-     // 리뷰 수정 알림
-
- public void removeReview(); 
-     // 리뷰 삭제 알림
-
- 
 //[식당예약]
- public void reservationConfirmationNotification();
+ public void reservationConfirmation();
      // 예약 확정 알림
  	// 사용자가 예약을 완료했을 때
  
- public void reservationChangeNotification(); 
+ public void reservationChange(); 
      // 예약 변경 알림
  	// 사용자가 예약을 변경할 경우
  
- public void reservationCancellationNotification(); 
+ public void reservationCancellation(); 
      // 예약 취소 알림
  	// 사용자가 예약을 취소할 경우
 
- public void firstComeReservationNotification();
+ public void firstComeReservation();
      // 선착순 예약 알림
  	// 식당에서 선착순 예약 서비스를 제공할 때, 예약이 가능해졌을 때 알림보내줌
 
@@ -68,19 +53,19 @@ public interface NotifyControllerImp {
      // 시간대별 혼잡도 알림 로직 구현
  	// 식당의 시간대별 혼잡도를 알림으로 제공
 
- public void restaurantOpenNotification(); 
+ public void restaurantOpen(); 
  	// 영업시간 확인 가능
  
 
 //[포장 주문]
- public void orderNotification();
+ public void pickUpOrder();
      // 주문 등록 알림 
 
- public void orderConfirmationNotification(); 
+ public void pickUpOrderConfirmation(); 
      // 주문 재확인 알림
  	// 사용자가 주문한 음식의 내용을 알림으로 보내주어 사용자가 올바른 주문을 했는지 확인
  
- public void orderCancelNotification();
+ public void pickUpOrderCancel();
      // 주문 취소 알림 
  	// 사용자가 주문한 음식을 취소했을 때
  
