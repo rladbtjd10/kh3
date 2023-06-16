7f0e6bcb34444bfb677bce63f7e6a2e5bf6050ab
wndud6328 <133620265+wndud6328@users.noreply.github.com>
2023-06-16 13:47:35 +0900

    Add files via upload

--- Kh3/src/model/Restaurant.java
+++ Kh3/src/model/Restaurant.java
@@ -1,5 +1,14 @@
-package model;
+package Alarm.Restaurant;
+
+import java.util.List;
 
 public class Restaurant {
 
+	private List waiting;	     // 웨이팅 리스트
+	private int time;	 	     // 예상시간
+	private int update;		 	 // 예상시간 업데이트
+	private int tableNum; 	 	 // 테이블 번호
+	private String tableMan;  	 // 테이블 관리
+	private String reserveMan;   // 예약 관리
+	private String alarm; 	     // 알림
 }
