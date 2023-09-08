33571e082c0c17212e9aff5d4dbafae4ab292f5a
Shin soomin <soomin6372@gmail.com>
2023-09-08 16:43:27 +0900

    feat_초기설정

---
+++ Kh3-backend/kh3/src/test/java/com/kh/countingBell/Kh3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.countingBell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kh3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
